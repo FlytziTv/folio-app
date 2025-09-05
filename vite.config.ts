62dc8646c3db60791ddea3aeb16a8470bb46b2ce
FlytziTv <flytzitv@gmail.com>
2025-09-05 13:33:37 +0200

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.NODE_ENV === "production" ? "/folio-app/" : "/", // ✅
+  base: "/", // ✅ IMPORTANT : site servi à la racine (alexisdjs.dev)
 });
