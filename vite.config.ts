e50db53b7506a407601f6a9ca4a3684fea002281
FlytziTv <flytzitv@gmail.com>
2025-09-05 13:48:04 +0200

    Fix: correct base and router for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/folio-app/", // ✅ obligatoire pour GitHub Pages
+  base: process.env.NODE_ENV === "production" ? "/folio-app/" : "/", // ✅
 });
